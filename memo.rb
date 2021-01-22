7e18fa97a9ee0a4acca40d948c914a0ab1ff8a34
EC2 Default User <ec2-user@ip-172-31-36-136.us-west-2.compute.internal>
2021-01-22 15:05:57 +0000

    add CSV

--- memo.rb
+++ memo.rb
@@ -2,9 +2,31 @@ require "csv"
 
 puts "1(新規でメモを作成) 2(既存のメモ編集する)"
 
-memo_type = gets.to_s
+memo_number = gets.to_i
 
-if selectNumber == 1
+if memo_number == 1
     puts "拡張子で書いたファイルを入力してください"
-    file_name = gets
-    file = File.open("#{file_name}.csv", "w") do |csv|
+    file_name = gets.chomp
+    puts "メモしたい内容を記入してください"
+    puts "完了したら、ctrl+Dを押してください"
+    
+    content = readlines(chomp: true)
+    CSV.open("#{file_name}.csv", "w")do |csv|
+    csv << content
+end
+
+elsif memo_number == 2
+    puts "既存メモの拡張子を除いたファイル名を入力してください"
+    file_name = gets.chomp
+    puts "追記したい内容を記入してください"
+    puts "完了したらctrl+Dを押してください"
+    
+    content = readlines(chomp: true)
+    CSV.open("#{file_name}.csv", "a")do |csv|
+    csv << content
+end
+
+else
+    puts "1,2の数字以外が入力されました。再度ファイルを実行後。1,2のどちらかを入力してください"
+
+end
